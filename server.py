0286718770b592c4e41c4b55ae92e9340d75bd5c
cguz14 <cguz14@gmail.com>
2023-10-05 19:32:09 -0500

    server base created

--- server.py
+++ server.py
@@ -1,18 +1,22 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 from pprint import pformat, pprint
 import os
-import requests
 
 app = Flask(__name__)
 app.secret_key = os.environ['SECRETKEY']
 
-
-
+app.config['ORESERVE_CONTEXT_ON_EXCEPTION'] = True
 
 # Replace this with routes and view functions!
+
+@app.route('/')
+def homepage():
+    """Show homepage"""
+
+    return render_template('homepage.html')
 
 
 if __name__ == "__main__":
