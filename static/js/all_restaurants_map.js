df35911ba5b5b3f2348449bd9989f9e310323c33
cguz14 <cguz14@gmail.com>
2023-11-02 22:26:13 -0500

    color/backgrounds addded to base. markerclusterers solved and added

--- static/js/all_restaurants_map.js
+++ static/js/all_restaurants_map.js
@@ -4,12 +4,11 @@
 let map;
 
 async function initMap() {
-  
+
   // Start map near middle of US
   const position = { lat: 40, lng: -100 };
 
   // Request needed libraries.
-  //@ts-ignore
   const { Map, InfoWindow } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
@@ -17,68 +16,73 @@ async function initMap() {
   map = new Map(document.getElementById("restaurants-page-map"), {
     zoom: 4,
     center: position,
-    mapId: "DEMO_MAP_ID",
+    mapId: "All_Restaurants_Map",
   });
 
-    const restaurantInfo = new InfoWindow();
+  const restaurantInfo = new InfoWindow();
+  const markers = [];
 
-    fetch('/api/restaurants')
+  fetch('/api/restaurants')
     .then((response) => response.json())
     .then((restaurants) => {
-    for (const restaurant of restaurants) {
+      for (const restaurant of restaurants) {
         // Define the content of the infoWindow
         const restaurantInfoContent = `
-        <div>
-        <ul class="restaurant-info">
-            <li><b>Restaurant Name: </b>${restaurant.restaurant_name}</li>
-            <li><b>Restaurant Address: </b>${restaurant.restaurant_address}</li>
-        </ul>
-        </div>
+            <div>
+              <ul class="restaurant-info">
+                <li><b>Restaurant Name: </b>${restaurant.restaurant_name}</li>
+                <li><b>Restaurant Address: </b>${restaurant.restaurant_address}</li>
+              </ul>
+            </div>
         `;
 
         const restaurantMarker = new AdvancedMarkerElement({
-        map,
-        position: {
+          map,
+          position: {
             lat: restaurant.restaurant_latitude,
             lng: restaurant.restaurant_longitude,
-        },
-        content: buildMarker(restaurant),
-        title: `Restaurant: ${restaurant.restaurant_name}`,
+          },
+          content: buildMarker(restaurant),
+          title: `Restaurant: ${restaurant.restaurant_name}`,
         });
 
+        markers.push(restaurantMarker)
+
         restaurantMarker.addListener('click', () => {
-        restaurantInfo.close();
-        restaurantInfo.setContent(restaurantInfoContent);
-        restaurantInfo.open(map, restaurantMarker);
+          restaurantInfo.close();
+          restaurantInfo.setContent(restaurantInfoContent);
+          restaurantInfo.open(map, restaurantMarker);
         });
-    }
+
+      }
+
+      new markerClusterer.MarkerClusterer({ markers, map });
+
     })
     .catch(() => {
-    alert(`
-    We were unable to retrieve Restaurant data!!!
-    `);
+      alert('We were unable to retrieve Restaurant data!!!');
     });
 
 }
 
 function buildMarker(restaurant) {
-    
-    let restaurantImg = document.createElement("img")
-    restaurantImg.src = `https:${restaurant.restaurant_icon}`;
 
-    if (restaurant.restaurant_icon == `static/img/attachment-guys-diner-background.jpg`) {
-        restaurantImg.src = restaurant.restaurant_icon;
-    }
+  let restaurantImg = document.createElement("img")
+  restaurantImg.src = `https:${restaurant.restaurant_icon}`;
 
-    const content = document.createElement("div");
+  if (restaurant.restaurant_icon == `static/img/attachment-guys-diner-background.jpg`) {
+    restaurantImg.src = restaurant.restaurant_icon;
+  }
 
-    content.innerHTML = `
+  const content = document.createElement("div");
+
+  content.innerHTML = `
     <div>
-        <img src=${restaurantImg.src} alt="Image for ${restaurant.restaurant_name}" class="all-restaurant-markers">
+      <img src=${restaurantImg.src} alt="Image for ${restaurant.restaurant_name}" class="all-restaurant-markers">
     </div>
-    `;
-  
-    return content;
+  `;
+
+  return content;
 
 }
 
